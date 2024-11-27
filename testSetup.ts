a171a95e9d24836127aa14c63c127f5fb11f62a2
Agata Szczuka <agata.szczuka@gmail.com>
2024-11-27 23:11:48 +0100

    Clear tab data when tab is closed.

--- testSetup.ts
+++ testSetup.ts
@@ -1,10 +1,19 @@
 import { vi } from 'vitest'
 
 interface ChromeMock {
+  runtime: {
+    onMessage: {
+      addListener: ReturnType<typeof vi.fn>
+      removeListener: ReturnType<typeof vi.fn>
+    }
+    getURL: ReturnType<typeof vi.fn>
+    getContexts: ReturnType<typeof vi.fn>
+  }
   storage: {
     local: {
       set: ReturnType<typeof vi.fn>
       get: ReturnType<typeof vi.fn>
+      remove: ReturnType<typeof vi.fn>
       onChanged: {
         addListener: ReturnType<typeof vi.fn>
         removeListener: ReturnType<typeof vi.fn>
@@ -13,14 +22,27 @@ interface ChromeMock {
   }
   tabs: {
     query: ReturnType<typeof vi.fn>
+    onRemoved: {
+      addListener: ReturnType<typeof vi.fn>
+      removeListener: ReturnType<typeof vi.fn>
+    }
   }
 }
 
 const mockChromeApi: ChromeMock = {
+  runtime: {
+    onMessage: {
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+    },
+    getURL: vi.fn(),
+    getContexts: vi.fn(() => Promise.resolve([])),
+  },
   storage: {
     local: {
       set: vi.fn(() => Promise.resolve()),
       get: vi.fn(() => Promise.resolve({ displayWarnings: true })),
+      remove: vi.fn(() => Promise.resolve()),
       onChanged: {
         addListener: vi.fn(),
         removeListener: vi.fn(),
@@ -29,6 +51,10 @@ const mockChromeApi: ChromeMock = {
   },
   tabs: {
     query: vi.fn(() => Promise.resolve([{ id: 123 }])),
+    onRemoved: {
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+    },
   },
 }
 
